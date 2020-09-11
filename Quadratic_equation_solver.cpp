221feffb613125658895cb42f3a18952ac4386fa
klorman <38407912+klorman@users.noreply.github.com>
2020-09-11 17:28:46 +0300

    Update Quadratic_equation_solver.cpp

--- Quadratic_equation_solver.cpp
+++ Quadratic_equation_solver.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <iostream>
 #include <math.h>
+#include <assert.h>
 
 enum roots {
-	inf, linear_1, linear_0, quadratic_2, quadratic_1, quadratic_0
+	INF, LINEAR_1, LINEAR_0, QUADRATIC_2, QUADRATIC_1, QUADRATIC_0
 };
 
 int input(double* coeff) {
@@ -15,71 +15,82 @@ int input(double* coeff) {
 			return 1;
 		}
 	}
-	return 0;
 }
 
 int linear_equation_solver(double** x1, double** x2, double b, double c) {
 	if ((b == 0) && (c == 0))
-		return inf;
+		return INF;
 	if (b != 0) {
+		assert(b != 0);
+
 		**x1 = (-c / b);
-		return linear_1;
+		return LINEAR_1;
 	}
 	else
-		return linear_0;
+		return LINEAR_0;
 }
 
 int quadratic_equation_solver(double a, double b, double c, double* x1, double* x2) {
-	if (a == 0)
-		return linear_equation_solver(&x1, &x2, b, c);
+	if (a == 0) return linear_equation_solver(&x1, &x2, b, c);
 
 	if (c == 0) {
 		*x1 = 0;
 		if (b == 0) {
 			*x2 = 0;
-			return quadratic_1;
+			return QUADRATIC_1;
 		}
+
+		assert(a != 0);
+
 		*x2 = -b / a;
-		return quadratic_2;
+		return QUADRATIC_2;
 	}
 
 	double Disc = b * b - 4 * a * c;
 
 	if (Disc > 0) {
-		*x1 = (-b + sqrt(Disc)) / (2 * a);
-		*x2 = (-b - sqrt(Disc)) / (2 * a);
+		assert(a != 0);
 
-		return(quadratic_2);
+		double sqrtD = sqrt(Disc);
+
+		*x1 = (-b + sqrtD) / (2 * a);
+		*x2 = (-b - sqrtD) / (2 * a);
+
+		return(QUADRATIC_2);
 	}
 	if (Disc == 0) {
-		*x1 = (-b + sqrt(Disc)) / (2 * a);
+		assert(a != 0);
 
-		return(quadratic_1);
+		double sqrtD = sqrt(Disc);
+
+		*x1 = (-b + sqrtD) / (2 * a);
+
+		return(QUADRATIC_1);
 	}
 
-	return quadratic_0;
+	return QUADRATIC_0;
 }
 
 void print_roots(double* coeff) {
 	double x1 = 0, x2 = 0;
 
 	switch (quadratic_equation_solver(coeff[0], coeff[1], coeff[2], &x1, &x2)) {
-	case inf:
+	case INF:
 		printf("Equation has an infinite number of solutions");
 		break;
-	case linear_1:
+	case LINEAR_1:
 		printf("Equation is linear. It has an only one solution.\n%g", x1);
 		break;
-	case linear_0:
+	case LINEAR_0:
 		printf("Equation is linear. It has no solutions.");
 		break;
-	case quadratic_2:
+	case QUADRATIC_2:
 		printf("Equation has two solutions\n%g and %g", x1, x2);
 		break;
-	case quadratic_1:
+	case QUADRATIC_1:
 		printf("Equation has two similar solutions\n%g", x1);
 		break;
-	case quadratic_0:
+	case QUADRATIC_0:
 		printf("Equation has no solutions");
 		break;
 	default:
@@ -89,13 +100,13 @@ void print_roots(double* coeff) {
 }
 
 int main() {
-	double* coeff = (double*)calloc(3, sizeof(double));
+	double* coeff = new double[3];
 
 	if (input(coeff) == 1)
 		return 0;
 
 	print_roots(coeff);
 
-	free(coeff);
+	delete[] coeff;
 	return 0;
 }
